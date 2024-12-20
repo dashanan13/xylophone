1b23adcc3c424dff225233e8e99b59a03d309de8
Mohit Sharma <mohit13@outlook.com>
2024-12-20 08:23:07 +0100

    Removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class Xylophone extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false, //disabled debug banner
       home: Scaffold(
         appBar: AppBar(
           title: const Text(
